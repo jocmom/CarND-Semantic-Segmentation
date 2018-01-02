2009d8b5d31c4ca81d683c63fe38121160af4ba2
jocmom <jombi@web.de>
2018-01-02 16:21:11 +0100

    ADD: 1x1 convolutions

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import project_tests as tests
 
 
 # Check TensorFlow Version
-assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+assert LooseVersion(tf.__version__) >= LooseVersion('1.0'),\
+    'Please use TensorFlow version 1.0 or newer.\
+    You are using {}'.format(tf.__version__)
 print('TensorFlow Version: {}'.format(tf.__version__))
 
 # Check for a GPU
 if not tf.test.gpu_device_name():
-    warnings.warn('No GPU found. Please use a GPU to train your neural network.')
+    warnings.warn(
+        'No GPU found. Please use a GPU to train your neural network.')
 else:
     print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
@@ -24,8 +27,6 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -33,6 +34,7 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
 
+    #   Use tf.saved_model.loader.load to load the model and weights
     tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
 
     graph = tf.get_default_graph()
@@ -41,8 +43,10 @@ def load_vgg(sess, vgg_path):
     vgg_layer3 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
     vgg_layer4 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
     vgg_layer7 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
-    
+
     return vgg_input, vgg_keep, vgg_layer3, vgg_layer4, vgg_layer7
+
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -55,8 +59,43 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
+    initializer = tf.contrib.layers.xavier_initializer()
+    regularizer = tf.contrib.layers.l2_regularizer(1e-3)
+    # regularizer = tf.random_normal_initializer(stddev=0.01)
+
+    # 1x1 convolutions of layer 7, 4, 3
+    layer7_conv_1x1 = tf.layers.conv2d(
+        vgg_layer7_out,
+        num_classes,
+        1,
+        strides=(1, 1),
+        padding='same',
+        kernel_initializer=initializer,
+        kernel_regularizer=regularizer)
+
+    layer4_conv_1x1 = tf.layers.conv2d(
+        vgg_layer4_out,
+        num_classes,
+        1,
+        strides=(1, 1),
+        padding='same',
+        kernel_initializer=initializer,
+        kernel_regularizer=regularizer)
+
+    layer3_conv_1x1 = tf.layers.conv2d(
+        vgg_layer3_out,
+        num_classes,
+        1,
+        strides=(1, 1),
+        padding='same',
+        kernel_initializer=initializer,
+        kernel_regularizer=regularizer)
+
+    # upsampling of these layer 7
+
     return None
+
+
 tests.test_layers(layers)
 
 
@@ -71,6 +110,8 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     # TODO: Implement function
     return None, None, None
+
+
 tests.test_optimize(optimize)
 
 
@@ -91,6 +132,8 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     """
     # TODO: Implement function
     pass
+
+
 tests.test_train_nn(train_nn)
 
 
@@ -112,7 +155,8 @@ def run():
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+        get_batches_fn = helper.gen_batch_function(
+            os.path.join(data_dir, 'data_road/training'), image_shape)
 
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
